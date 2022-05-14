9337aa8eae5265d6889e99c30ea07a24af7c9b5f
Anssi Lappalainen <anssilap@users.noreply.github.com>
2022-05-14 22:14:15 +0300

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,9 +9,16 @@ if ! which ansible &> /dev/null
         sudo pacman -S --noconfirm ansible
 fi
 
+# Make sure community.general collection is installed
 if ! ansible-galaxy collection verify community.general | grep "Installed collection found at" &> /dev/null
     then
         ansible-galaxy collection install community.general
+fi
+
+# Make sure community.general collection is installed
+if ! ansible-galaxy collection verify community.crypto | grep "Installed collection found at" &> /dev/null
+    then
+        ansible-galaxy collection install community.crypto
 fi
 
 ansible-playbook bootstrap.yml -i inventory.yml -K --connection=local
